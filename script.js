ae175394cafbfa20867206e0b6578424e0defd88
Jorge Santamaria <jolusantamaria@gmail.com>
2025-03-12 10:56:14 -0600

    Por fin iniciando con algo de verdad

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function getComputerChoice(){
-    let computerChoice = Math.random()
-    console.log(computerChoice)
+//------Funcion para tomar la decision de manera aleatorea
+function getComputerChoice() {
+    let randomNum = Math.random() * 100; // Número entre 0 y 100
+    let choice;
+
+    if (randomNum <= 33) {
+        choice = "Rock";
+    } else if (randomNum >= 68) {
+        choice = "Paper";
+    } else {
+        choice = "Scissors";
+    }
+
+    return choice;
+
 }
+console.log(getComputerChoice()); // Prueba la función
+
+//----------Funcion para tomar la decision humana
+
+function getHumanChoice() {
+    let HumanChoice = prompt("Ingresa algo");
+    return HumanChoice;
+}
+console.log(getHumanChoice());
