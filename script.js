58b2c6bcb32e53cf5d9f74336d61a0b5e18ad0cf
Jorge Santamaria <jolusantamaria@gmail.com>
2025-03-14 13:10:29 -0600

    Nueva funcion

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
+let humanScore = 0;
+let computerScore = 0;
 //------Funcion para tomar la decision de manera aleatorea
 function getComputerChoice() {
     let randomNum = Math.random() * 100; // Número entre 0 y 100
-    let choice;
+    let computerChoice;
 
     if (randomNum <= 33) {
-        choice = "Rock";
+        computerChoice = "Piedra";
     } else if (randomNum >= 68) {
-        choice = "Paper";
+        computerChoice = "Papel";
     } else {
-        choice = "Scissors";
+        computerChoice = "Tijeras";
     }
-
-    return choice;
-
+    return computerChoice;
 }
-console.log(getComputerChoice()); // Prueba la función
+//console.log("Comp01", getComputerChoice()); // Prueba la función
 
 //----------Funcion para tomar la decision humana
-
 function getHumanChoice() {
-    let HumanChoice = prompt("Ingresa algo");
-    return HumanChoice;
+    let humanChoice = prompt("Ingresa: Piedra, Papel o Tijeras");
+    return humanChoice;
 }
-console.log(getHumanChoice());
+//console.log("Hum01", getHumanChoice()); // Prueba la función
+
+function playRound(computerChoice, humanChoice) {
+    return console.log(computerChoice, humanChoice);
+}
